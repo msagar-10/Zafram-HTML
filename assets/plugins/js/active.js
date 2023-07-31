ef1c8bb9674f29bcd88467a881882a2b3896e992
msagar-10 <mhossainsagar@gmail.com>
2023-07-31 19:29:58 +0600

    add:main

--- assets/plugins/js/active.js
+++ assets/plugins/js/active.js
@@ -58,27 +58,6 @@
   	===============================================================================*/
     $("select").niceSelect();
 
-    /*================================================================================
-			Onepage Nav JS
-		==================================================================================*/
-    $("#nav, #mobile-nav").onePageNav({
-      currentClass: "active",
-      changeHash: false,
-      scrollSpeed: 750,
-      scrollThreshold: 0.5,
-      filter: "",
-      easing: "swing",
-      begin: function () {
-        //I get fired when the animation is starting
-      },
-      end: function () {
-        //I get fired when the animation is ending
-      },
-      scrollChange: function ($currentListItem) {
-        //I get fired when you enter a section and I pass the list item of the section
-      },
-    });
-
     /*==============================================================================
 		  Wow JS
 	  ================================================================================*/
